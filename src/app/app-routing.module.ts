c7d42d5ae1e305aff8b2bfcf2e4085e507c822c1
Muhammad Fahid <fahidali333123@gmail.com>
2025-07-03 17:22:23 +0500

    home page design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AllDoctorsComponent } from './all-doctors/all-doctors.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path:'all-doctors' , component: AllDoctorsComponent}
+  {path:'all-doctors' , component: AllDoctorsComponent},
+  {path:'' , component: HomeComponent}
+
 ];
 
 @NgModule({
